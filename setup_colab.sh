866b54a34061ee93076a57736ac3715720696af2
Vibha Masti <vsmasti@gmail.com>
2024-02-04 13:09:49 -0500

    Added setup script

---
+++ setup_colab.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+pip install torchmetrics==0.4.0
+pip install torchtext==0.6.0
+pip install -e .
+pip install --upgrade torch torchvision
+pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
+pip install taming-transformers-rom1504
+pip install kornia
